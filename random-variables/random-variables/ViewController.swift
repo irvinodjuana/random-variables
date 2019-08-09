9e52313269168b514af779d1d24b41b7b1ef0b68
Irvino Djuana <irvino.djuana@gmail.com>
2019-08-09 12:49:33 -0700

    Initial commit for Xcode files

---
+++ random-variables/random-variables/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  random-variables
+//
+//  Created by Irvino Djuana on 2019-08-09.
+//  Copyright © 2019 Irvino Djuana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
