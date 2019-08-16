0d897d251b00567747d309f89ab661f626e8eeda
Irvino Djuana <irvino.djuana@gmail.com>
2019-08-16 13:51:24 -0700

    added continuous and discrete buttons for UI

--- random-variables/random-variables/ViewController.swift
+++ random-variables/random-variables/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func bernoulliSelected(_ sender: Any) {
+        print("Bernoulli!")
+    }
+    
 }
 
