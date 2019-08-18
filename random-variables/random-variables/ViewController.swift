df9c0e6094672951976e990155d2030423c7dbaa
Irvino Djuana <irvino.djuana@gmail.com>
2019-08-18 16:21:20 -0700

    Set up view controller layout in storyboard

--- random-variables/random-variables/ViewController.swift
+++ random-variables/random-variables/ViewController.swift
@@ -14,10 +14,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func bernoulliSelected(_ sender: Any) {
-        print("Bernoulli!")
-    }
     
 }
 
