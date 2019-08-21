09a841d6678460fb181a45fd7bacea4ea8930928
Irvino Djuana <irvino.djuana@gmail.com>
2019-08-21 16:11:47 -0700

    Accidentally commited Pods directory

--- random-variables/Pods/Target Support Files/Pods-random-variables/Pods-random-variables-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_random_variables : NSObject
-@end
-@implementation PodsDummy_Pods_random_variables
-@end
--- random-variables/Pods/Target Support Files/Pods-random-variablesTests/Pods-random-variablesTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_random_variablesTests : NSObject
-@end
-@implementation PodsDummy_Pods_random_variablesTests
-@end
